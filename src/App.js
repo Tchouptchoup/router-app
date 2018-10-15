799ed27e1706c735ad8571f7c8de9e659fc34ea0
Maitetxu Darguy <maitetxu.darguy@gmail.com>
2018-10-15 11:48:55 +0200

    router app challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
+
+import Home from "./Home"
+import Histoire from "./History"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <NavLink exact to="/" activeStyle={{color: "red"}}>Accueil </NavLink>
+          <NavLink to="/notre-histoire" activeStyle={{color: "red"}}> Histoire</NavLink>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/notre-histoire" component={Histoire} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Histoire extends Component {
+    render() {
+        return (
+            <div>
+                <p>Notre histoire</p>
+            </div>
+        )
+    }
+};
+
+export default Histoire;
